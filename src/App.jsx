d30647064cc452a82759a0791e2c6a04f879d258
Siftab <siiffuuu@gmail.com>
2024-01-27 02:21:09 +0600

    main layout implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MainLayout from './mainLayout/MainLayout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-      <h1 className='text-6xl'>SparkIf</h1>
-     
-    </>
+    <div className='h-screen flex justify-center items-center'>
+       {/* <h1 className='text-6xl'>SparkIf</h1> */}
+      <MainLayout></MainLayout>
+    </div>
   )
 }
 
---
+++ src/mainLayout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div className='h-[90vh] w-[90vw] bg-black'>
+            this is main Layout 
+        </div>
+    );
+};
+
+export default MainLayout;
